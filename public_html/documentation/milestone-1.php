30f6555b3317d026a571e79bb9f394e3d1e494ff
John Johnson-Rodgers <john@johnthe.dev>
2019-10-10 21:34:42 -0600

    added purpose, audience, goal to milestone-1.php

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF8">
+	<title>Milestone 1</title>
+</head>
+<body>
+
+<div id="purpose">
+	<h3 id="purposeTitle" class="title">Purpose</h3>
+	<p id="purposeDescription" class="description">Creating my PWP will build my profile and allow me to practice and improve my web development skills.</p>
+</div>
+
+<div id="audience">
+	<h3 id="audienceTitle" class="title">Audience</h3>
+	<p id="audienceDescription" class="description">The site's audience will be local contractors and homeowners, specifically Andy Hart of Hart Construction. My intended audience includes my own potential employers who may view this site.</p>
+</div>
+
+<div id="goal">
+	<h3 id="goalTitle" class="title">Goal</h3>
+	<p id="goalDescription" class="description">My goals are to improve the online presence of Rodgers Plumbing and Heating and to showcase my work.</p>
+</div>
+</body>
+</html>
