417f009a87cd197e06c44119e27e8414a2504426
John Johnson-Rodgers <john@johnthe.dev>
2019-10-10 22:54:09 -0600

    added persona information from notes need to organize more still

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,20 +5,70 @@
 	<title>Milestone 1</title>
 </head>
 <body>
+<h1 id="pageTitle" class="title">Milestone 1: John Johnson-Rodgers</h1>
 
-<div id="purpose">
-	<h3 id="purposeTitle" class="title">Purpose</h3>
-	<p id="purposeDescription" class="description">Creating my PWP will build my profile and allow me to practice and improve my web development skills.</p>
+<div id="purposeAudienceGoalSection">
+	<h3 id="purposeAudienceGoalTitle" class="title">Purpose, Audience, Goal</h3>
+
+	<div id="purpose">
+		<h5 id="purposeTitle" class="title">Purpose</h5>
+		<p id="purposeDescription" class="description">Creating my PWP will build my profile and allow me to practice and improve my web development skills.</p>
+	</div>
+
+	<div id="audience">
+		<h5 id="audienceTitle" class="title">Audience</h5>
+		<p id="audienceDescription" class="description">The site's audience will be local contractors and homeowners, specifically Andy Hart of Hart Construction. My intended audience includes my own potential employers who may view this site.</p>
+	</div>
+
+	<div id="goal">
+		<h5 id="goalTitle" class="title">Goal</h5>
+		<p id="goalDescription" class="description">My goals are to improve the online presence of Rodgers Plumbing and Heating and to showcase my work.</p>
+	</div>
+
 </div>
 
-<div id="audience">
-	<h3 id="audienceTitle" class="title">Audience</h3>
-	<p id="audienceDescription" class="description">The site's audience will be local contractors and homeowners, specifically Andy Hart of Hart Construction. My intended audience includes my own potential employers who may view this site.</p>
-</div>
+	<div id="persona">
+		<h3 id="personaTitle" class="title">Persona: Andy Hart</h3>
 
-<div id="goal">
-	<h3 id="goalTitle" class="title">Goal</h3>
-	<p id="goalDescription" class="description">My goals are to improve the online presence of Rodgers Plumbing and Heating and to showcase my work.</p>
-</div>
+		<div id="personaBackground">
+			<h5 id="personaBackgroundTitle" class="title">Professional Background</h5>
+			<p id="personaBackgroundDescription" class="description">Andy Hart founded Hart Construction in August 1989
+				when he was 25 years old with his college friend Jim Pekin. They still own Hart Construction. Prior to that, he worked
+				on a ranch in Chromo, Colorado for two years. He has a BA in Studio Art and Art History from Georgetown University. He
+				just received a bid request on a project that is larger than most of the projects he has worked on in the past and is
+				selecting subcontractors to invite to submit bids to his construction firm.</p>
+		</div>
+
+		<div id="personaBackground">
+			<h5 id="personaTechnologyTitle" class="title">Technology</h5>
+			<p id="personaBackgroundDescription" class="description"></p>
+		</div>
+
+		<div id="personaBackground">
+			<h5 id="personaTechnologyTitle" class="title">Technology</h5>
+			<p id="personaBackgroundDescription" class="description"></p>
+		</div>
+
+				<div id="personaBackground">
+					<h5 id="personaTechnologyTitle" class="title">Technology</h5>
+		<p id="personaBackgroundDescription" class="description"></p>
+		<p id="personaDescription" class="description">Andy Hart: Andy Hart ia a fifty five year old white man currently
+			residing in Northwest Albuquerque, New Mexico off of Don Quixote Ct. Andy Hart founded Hart Construction in August 1989
+			when he was 25 years old with his college friend Jim Pekin. They still own Hart Construction. Prior to that, he worked
+			on a ranch in Chromo Colorado for two years. He has a BA in Studio Art and Art History from Georgetown University. His
+			father was a prominent lawyer in Albuquerque and has a building at the University of New Mexico named after him. Andy is
+			a serious, busy man who is very concise in his correspondence. He enjoys bass guitar.<br>
+		Andy is a windows user and would likely access the website while using Chrome or Internet Explorer on his desktop. He would
+			likely visit the website before inviting Rodgers Plumbing and Heating to bid on a project for Hart Construction to see if
+			they have worked on projects of a similar scope in the past. He may have previously dealt with subcontractors who were
+			unable to complete large projects on schedule. The goal of the website is to give him a good idea of the scale and variety
+			of jobs Rodgers Plumbing and Heating has performed along with a few testimonials commenting on the professionalism and
+			timeliness of Rodgers Plumbing and Heating.</p>
+	</div>
+
+	<div id="personaPicture" class="photo">
+		<img src="https://media.licdn.com/dms/image/C5603AQE9eYJc2uk6xQ/profile-displayphoto-shrink_200_200/0?e=1576108800&v=beta&t=mtGkuCq9c2K8yf7kqOa5Yt5xRpKFnVN40E_thJxA9p8" alt="Photo of Andy Hart">
+	</div>
+
 </body>
 </html>
