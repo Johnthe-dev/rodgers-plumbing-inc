fa0656ee60f19e6a66aa50c0c6cd5fa538e4d844
John Johnson-Rodgers <john@johnthe.dev>
2019-10-15 19:18:50 -0600

    finished styling for milestone-1.php

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -10,7 +10,7 @@
 
 <div id="purposeAudienceGoalSection">
 	<h3 id="purposeAudienceGoalTitle" class="title">Purpose, Audience, Goal</h3>
-
+<div class="flex-container">
 	<div id="purpose">
 		<h5 id="purposeTitle" class="subtitle">Purpose</h5>
 		<p id="purposeDescription" class="description">Creating my PWP will build my profile and allow me to practice and improve, and display my web development skills.</p>
@@ -35,41 +35,47 @@
 			<img src="foto-sushi-6anudmpILw4-unsplash.jpg" alt="Photo of Andy Smith">
 			<p>Photo by Foto Sushi on Unsplash</p>
 		</div>
+		<div class="flex-container">
+			<div id="personaBackground">
+				<h5 id="personaBackgroundTitle" class="subtitle">Personal Background</h5>
+				<p id="personaBackgroundDescription" class="description">Andy Smith ia a fifty five year old caucasian man currently
+					residing in Northwest Albuquerque. Andy Smith founded Smith Construction in August 1989
+					when he was 25 years old with his college friend Jim Paul. They still own Smith Construction. Prior to that, he worked
+					on a ranch in Chromo, Colorado for two years. He has a BA in Studio Art and Art History from Georgetown University. He
+					just received a bid request on a project that is larger than most of the projects he has worked on in the past and is
+					selecting subcontractors to invite to submit bids to his construction firm.</p>
+			</div>
 
-		<div id="personaBackground">
-			<h5 id="personaBackgroundTitle" class="subtitle">Personal Background</h5>
-			<p id="personaBackgroundDescription" class="description">Andy Smith ia a fifty five year old caucasian man currently
-				residing in Northwest Albuquerque. Andy Smith founded Smith Construction in August 1989
-				when he was 25 years old with his college friend Jim Paul. They still own Smith Construction. Prior to that, he worked
-				on a ranch in Chromo, Colorado for two years. He has a BA in Studio Art and Art History from Georgetown University. He
-				just received a bid request on a project that is larger than most of the projects he has worked on in the past and is
-				selecting subcontractors to invite to submit bids to his construction firm.</p>
+			<div id="personaTechnology">
+				<h5 id="personaTechnologyTitle" class="subtitle">Technology</h5>
+				<p id="personaTechnologyDescription" class="description">Andy uses an ergonomic keyboard, mouse and two monitors with his Dell XPS running Windows 10 Pro. He uses Google Chrome on the latest version to access the web.</p>
+			</div>
 		</div>
 
-		<div id="personaTechnology">
-			<h5 id="personaTechnologyTitle" class="subtitle">Technology</h5>
-			<p id="personaTechnologyDescription" class="description">Andy uses an ergonomic keyboard, mouse and two monitors with his Dell XPS running Windows 10 Pro. He uses Google Chrome on the latest version to access the web.</p>
+		<div class="flex-container">
+			<div id="personaAttitudes">
+				<h5 id="personaAttitudesTitle" class="subtitle">Attitudes and Behaviors</h5>
+				<p id="personaAttitudesDescription" class="description">Andy is a concise, exacting professional. He expects companies he works with to behave in the same manner. He appreciates functionality and clean design without distractions.</p>
+			</div>
+
+			<div id="personaFrustrations">
+				<h5 id="personaFrustrationsTitle" class="subtitle">Frustrations and Needs</h5>
+				<p id="personaFrustrationsDescription" class="description">Andy has previously dealt with subcontractors who were unable to complete large projects on schedule. He needs a number of qualified companies to send bid requests to so he has a wide selection to pick from.</p>
+			</div>
 		</div>
 
-		<div id="personaAttitudes">
-			<h5 id="personaAttitudesTitle" class="subtitle">Attitudes and Behaviors</h5>
-			<p id="personaAttitudesDescription" class="description">Andy is a concise, exacting professional. He expects companies he works with to behave in the same manner. He appreciates functionality and clean design without distractions.</p>
+		<div class="flex-container">
+			<div id="personaGoals">
+				<h5 id="personaGoalsTitle" class="subtitle">Goals</h5>
+				<p id="personaGoalsDescription" class="description">He wants to find a company who will give him the most reliable quality of work for the lowest cost that can work on large scale projects.</p>
+			</div>
+
+			<div id="personaStory">
+				<h5 id="personaStoryTitle" class="subtitle">User Story</h5>
+				<p id="personaStoryDescription" class="description">As a General Contractor, I need to find subcontractors who are cheap, reliable, and experienced to invite to bid on a construction project.</p>
+			</div>
 		</div>
 
-		<div id="personaFrustrations">
-			<h5 id="personaFrustrationsTitle" class="subtitle">Frustrations and Needs</h5>
-			<p id="personaFrustrationsDescription" class="description">Andy has previously dealt with subcontractors who were unable to complete large projects on schedule. He needs a number of qualified companies to send bid requests to so he has a wide selection to pick from.</p>
-		</div>
-
-		<div id="personaGoals">
-			<h5 id="personaGoalsTitle" class="subtitle">Goals</h5>
-			<p id="personaGoalsDescription" class="description">He wants to find a company who will give him the most reliable quality of work for the lowest cost that can work on large scale projects.</p>
-		</div>
-
-		<div id="personaStory">
-			<h5 id="personaStoryTitle" class="subtitle">User Story</h5>
-			<p id="personaStoryDescription" class="description">As a General Contractor, I need to find subcontractors who are cheap, reliable, and experienced to invite to bid on a construction project.</p>
-		</div>
 
 		<!--<p id="personaDescription" class="description">Andy Smith: Andy Smith ia a fifty five year old white man currently
 			residing in Northwest Albuquerque, New Mexico off of Don Quixote Ct. Andy Smith founded Smith Construction in August 1989
