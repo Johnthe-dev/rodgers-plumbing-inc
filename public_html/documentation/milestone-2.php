76e14f58a2396dc2b8e1604b96438799b8d0f7a6
John Johnson-Rodgers <john@johnthe.dev>
2019-10-19 16:11:08 -0600

    Edited subTitle class values in milestone-2.php

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -13,33 +13,33 @@
 		<p class="description">The site for Rodgers Plumbing and Heating will be on one page and have the following eight sections:</p>
 
 		<div id="heroImage">
-			<h5 class="subtitle" id="heroImageTitle">Hero Image</h5>
+			<h5 class="subtitle" id="heroImageTitle">1.  Hero Image</h5>
 			<p class="description" id="heroImageDescription">This will be the "Hero Image", a picture of a smiling RPHI employee wearing company branded PPE with a job site or building in the background. It will have the name of the company at the top of the image. It will fill the whole width of whatever viewport it is in. It will zoom in appropriately so that the employee's face is distinguishable.</p>
 		</div>
 
 		<div id="navigationBar">
-			<h5 class="subtitle" id="navigationBarTitle">Navigation Bar</h5>
+			<h5 class="subtitle" id="navigationBarTitle">2.  Navigation Bar</h5>
 			<p class="description" id="navigationBarDescription">This will initially be below the "Hero Image" and will remain at the top of the screen as the viewer scrolls down(as seen in the wireframes below). This will have the company logo on the left side of the bar and navigational links on the right side of the bar. These links will include a "About Us" link(links to Call To Action and Company History), a "Services" link (links to Scroller and Services We Provide)  and a "Contact" link(links to Contact Form and Location and Accreditation. When clicked, the viewport will be moved to the appropriate section of the web page.</p>
 		</div>
 
 		<div id="callToAction">
-			<h5 class="subtitle" id="callToActionTitle">Call to Action</h5>
+			<h5 class="subtitle" id="callToActionTitle">3.  Call to Action</h5>
 			<p class="description" id="callToActionDescription">This section will include a photo on the left and a text block on the right. The photo will be of a few employees working on a project. the text block will state why RPHI does what it does and a call to action to "considering using us for your next plumbing project, big or small".</p>
 		</div>
 
 		<div id="companyHistory">
-			<h5 class="subtitle" id="companyHistoryTitle">Company History</h5>
+			<h5 class="subtitle" id="companyHistoryTitle">4.  Company History</h5>
 			<p class="description" id="companyHistoryDescription">This section will include a photo on the right and a text block on the left. The text block will include a founding story, number of years active, and an introduction to Jay Rodgers. The photo will be of Jay Rodgers smiling, perhaps with the company building in the background.</p>
 		</div>
 
 		<div id="scroller">
-			<h5 class="subtitle" id="scrollerTitle">Scroller</h5>
+			<h5 class="subtitle" id="scrollerTitle">5.  Scroller</h5>
 			<p class="description" id="scrollerDescription">This section will be a scroller with images of various types of work that the company performs. eg. utility work, plumbing work, fixture installation, and residential work. This is also an opportunity to show the wide varieties of scale of project that we work on. This section will take up the width of the viewport and will adjust to fit smaller screens.</p>
 		</div>
 
 		<div id="servicesWeProvide">
-			<h5 class="subtitle" id="servicesWeProvideTitle">Services We Provide</h5>
-			<p class="description" id="servicesWeProvideDescription">This is the first of two sections that will be different on mobile versus on desktop or tablet. </p>
+			<h5 class="subtitle" id="servicesWeProvideTitle">6.  Services We Provide</h5>
+			<p class="description" id="servicesWeProvideDescription">This section will be different on mobile versus on desktop or tablet. </p>
 		</div>
 	</div>
 
