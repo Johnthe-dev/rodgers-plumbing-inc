a932e1355cc8afccc06c4a98cf6a8df4d4a66f5d
John Johnson-Rodgers <john@johnthe.dev>
2019-10-19 14:53:11 -0600

    Created milestone-2.php Added HTML boiler plate, pageTitle, contentStrategy,and heroImage

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF8">
+	<title>Milestone 1</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1 class="title" id="pageTitle">Personal Web Page Milestone 2</h1>
+<h3 class="title" id="contentStrategyTitle">Content Strategy</h3>
+<p class="description">The site for Rodgers Plumbing and Heating will be on one page and have the following eight sections</p>
+<h5 class="subtitle" id="heroImageTitle">Hero Image</h5>
+<p class="description" id="heroImageDescription"></p>
+
+</body>
+</html>
