6b7153578706e944223c1b02f9381961ec877558
John Johnson-Rodgers <john@johnthe.dev>
2019-10-19 15:39:21 -0600

    Added companyHistory to milestone-2.php

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,30 +6,36 @@
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-<h1 class="title" id="pageTitle">Personal Web Page Milestone 2</h1>
+	<h1 class="title" id="pageTitle">Personal Web Page Milestone 2</h1>
 
-<div id="contentStrategyTitle">
-	<h3 class="title" id="contentStrategyTitle">Content Strategy</h3>
-	<p class="description">The site for Rodgers Plumbing and Heating will be on one page and have the following eight sections:</p>
-</div>
+	<div id="contentStrategy">
+		<h3 class="title" id="contentStrategyTitle">Content Strategy</h3>
+		<p class="description">The site for Rodgers Plumbing and Heating will be on one page and have the following eight sections:</p>
 
-<div id="heroImage">
-	<h5 class="subtitle" id="heroImageTitle">Hero Image</h5>
-	<p class="description" id="heroImageDescription">This will be the "Hero Image", a picture of a smiling RPHI employee wearing company branded PPE with a job site or building in the background. It will have the name of the company at the top of the image. It will fill the whole width of whatever viewport it is in. It will zoom in appropriately so that the employee's face is distinguishable.</p>
-</div>
+		<div id="heroImage">
+			<h5 class="subtitle" id="heroImageTitle">Hero Image</h5>
+			<p class="description" id="heroImageDescription">This will be the "Hero Image", a picture of a smiling RPHI employee wearing company branded PPE with a job site or building in the background. It will have the name of the company at the top of the image. It will fill the whole width of whatever viewport it is in. It will zoom in appropriately so that the employee's face is distinguishable.</p>
+		</div>
 
-<div id="navigationBar">
-	<h5 class="subtitle" id="navigationBarTitle">Navigation Bar</h5>
-	<p class="description" id="navigationBarDescription">This will initially be below the "Hero Image" and will remain at the top of the screen as the viewer scrolls down(as seen in the wireframes below). This will have the company logo on the left side of the bar and navigational links on the right side of the bar. These links will include a "About Us" link(links to Call To Action and Company History), a "Services" link (links to Scroller and Services We Provide)  and a "Contact" link(links to Contact Form and Location and Accreditation. When clicked, the viewport will be moved to the appropriate section of the web page.</p>
-</div>
+		<div id="navigationBar">
+			<h5 class="subtitle" id="navigationBarTitle">Navigation Bar</h5>
+			<p class="description" id="navigationBarDescription">This will initially be below the "Hero Image" and will remain at the top of the screen as the viewer scrolls down(as seen in the wireframes below). This will have the company logo on the left side of the bar and navigational links on the right side of the bar. These links will include a "About Us" link(links to Call To Action and Company History), a "Services" link (links to Scroller and Services We Provide)  and a "Contact" link(links to Contact Form and Location and Accreditation. When clicked, the viewport will be moved to the appropriate section of the web page.</p>
+		</div>
 
-<div id="callToAction">
-	<h5 class="subtitle" id="callToActionTitle">Call to Action</h5>
-	<p class="description" id="callToActionDescription">This section will include a photo on the left and a text block on the right. The photo will be of a few employees working on a project. the text block will state why RPHI does what it does and a call to action to "considering using us for your next plumbing project, big or small".</p>
-</div>
+		<div id="callToAction">
+			<h5 class="subtitle" id="callToActionTitle">Call to Action</h5>
+			<p class="description" id="callToActionDescription">This section will include a photo on the left and a text block on the right. The photo will be of a few employees working on a project. the text block will state why RPHI does what it does and a call to action to "considering using us for your next plumbing project, big or small".</p>
+		</div>
 
-<div id="companyHistory">
+		<div id="companyHistory">
+			<h5 class="subtitle" id="companyHistoryTitle">Company History</h5>
+			<p class="description" id="companyHistoryDescription">This section will include a photo on the right and a text block on the left. The text block will include a founding story, number of years active, and an introduction to Jay Rodgers. The photo will be of Jay Rodgers smiling, perhaps with the company building in the background.</p>
+		</div>
 
-</div>
+		<div id="Scroller">
+
+		</div>
+	</div>
+
 </body>
 </html>
