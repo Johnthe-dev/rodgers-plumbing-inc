085a9616c52830e25dc704ae09065c0b823e1bc9
John Johnson-Rodgers <john@johnthe.dev>
2019-10-19 15:11:06 -0600

    Added navigationBar to milestone-2.php

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,10 +7,24 @@
 </head>
 <body>
 <h1 class="title" id="pageTitle">Personal Web Page Milestone 2</h1>
-<h3 class="title" id="contentStrategyTitle">Content Strategy</h3>
-<p class="description">The site for Rodgers Plumbing and Heating will be on one page and have the following eight sections</p>
-<h5 class="subtitle" id="heroImageTitle">Hero Image</h5>
-<p class="description" id="heroImageDescription"></p>
 
+<div id="contentStrategyTitle">
+	<h3 class="title" id="contentStrategyTitle">Content Strategy</h3>
+	<p class="description">The site for Rodgers Plumbing and Heating will be on one page and have the following eight sections:</p>
+</div>
+
+<div id="heroImage">
+	<h5 class="subtitle" id="heroImageTitle">Hero Image</h5>
+	<p class="description" id="heroImageDescription">This will be the "Hero Image", a picture of a smiling RPHI employee wearing company branded PPE with a job site or building in the background. It will have the name of the company at the top of the image. It will fill the whole width of whatever viewport it is in. It will zoom in appropriately so that the employee's face is distinguishable.</p>
+</div>
+
+<div id="navigationBar">
+	<h5 class="subtitle" id="navigationBarTitle">Navigation Bar</h5>
+	<p class="description" id="navigationBarDescription">This will initially be below the "Hero Image" and will remain at the top of the screen as the viewer scrolls down(as seen in the wireframes below). This will have the company logo on the left side of the bar and navigational links on the right side of the bar. These links will include a "About Us" link, a "Services" link and a "Contact" link. When clicked, the viewport will be moved to the appropriate section of the web page.</p>
+</div>
+
+<div id="callToAction">
+
+</div>
 </body>
 </html>
