77cb12ca163e06ac2edf950cd7ebaf750bff34d8
John Johnson-Rodgers <john@johnthe.dev>
2019-11-04 20:24:17 -0700

    started second section, the navbar

--- index.php
+++ index.php
@@ -8,36 +8,57 @@
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
 			integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	<link rel="stylesheet" href="styles.css">
 
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-			  integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"
+			  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
 			  crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
-			  integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+			  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
 			  crossorigin="anonymous"></script>
-<div class="container-fluid">
-	<div class="row" id="box-search">
-		<div class="thumbnail text-center">
-			<img src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="" class="img-responsive">
-			<div class="caption">
-				<p>Rodgers Plumbing and Heating</p>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+			  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+			  crossorigin="anonymous"></script>
+	<div class="container-fluid">
+		<div class="row" id="box-search">
+			<div class="thumbnail text-center">
+				<img src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="picture of construction worker on a jobsite"
+					  class="img-responsive">
+				<div class="caption">
+					<p>Rodgers Plumbing and Heating</p>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
 	<title id="top">Rodgers Plumbing and Heating</title>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-prime sticky-top">
-	<a href="#top"><img class="navbar-brand" src="http://rphi.biz/images/rphi001002.png" alt="Logo for Rodgers Plumbing and Heating"/></a>
+	<a href="#top"><img class="navbar-brand" src="http://rphi.biz/images/rphi001002.png"
+		alt="Logo for Rodgers Plumbing and Heating"/></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+			  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-link">Rodgers Plumbing and Heating</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="#">About Us <span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Services</a>
+			</li>
+
+			<li class="nav-item">
+				<a class="nav-link" href="#">Contact</a>
+			</li>
+		</ul>
+	</div>
 
 </nav>
-
-
-
-
 
 
 </body>
