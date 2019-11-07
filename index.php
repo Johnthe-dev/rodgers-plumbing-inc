32525836161ae3b86350c44cd850e9b9e6f999c0
John Johnson-Rodgers <john@johnthe.dev>
2019-11-07 08:15:54 -0700

    tweaking style, added carousal

--- index.php
+++ index.php
@@ -48,8 +48,10 @@
 
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
-	<a class="navbar-brand" href="#top"><img src="http://rphi.biz/images/rphi001002.png" alt="logo for Rodgers Plumbing and Heating"></a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	<a class="navbar-brand" href="#top"><img src="http://rphi.biz/images/rphi001002.png"
+														  alt="logo for Rodgers Plumbing and Heating"></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+			  aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
 	<div class="collapse navbar-collapse" id="navbarNav">
@@ -66,7 +68,93 @@
 		</ul>
 	</div>
 </nav>
+<div class="container">
+	<div class="row pt-5">
+		<div class="col-md-6">
+			<img src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="Stand-In Picture">
+		</div>
+		<div class="col-md-6">
+			<p>We are a locally owned and operated plumbing and heating company serving Albuquerque, Santa Fe and the rest
+				of the great state of New Mexico. We complete projects ranging from small home repair to large hospital
+				construction sites. Consider using Rodgers Plumbing and Heating for your next Plumbing project, large or
+				small.</p>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row align-items-center pt-5">
+		<div class="col-md-6">
+			<p>Rodgers Plumbing and Heating was founded in 1973 by life-long Albuquerque native Jim Rodgers. Since then,
+				Rodgers Plumbing and Heating has grown as a diverse, community oriented plumbing company. We participate in
+				many local apprenticeship programs and train our workers to be professional, safe, and hard working. We have
+				proudly served New Mexico with high quality, competitively priced plumbing services for over forty years and
+				hope to continue to do so for many more.</p>
+		</div>
+		<div class="col-md-6">
+			<img src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="Stand-In Picture">
+		</div>
+	</div>
+</div>
 
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+	<ol class="carousel-indicators">
+		<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+		<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+		<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+		<li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+	</ol>
+	<div class="carousel-inner">
+		<div class="carousel-item.active">
+			<img class="d-block w-100" src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="First slide">
+			<div class="carousel-caption d-none d-md-block">
+				<h5>Services We Provide</h5>
+				<p>Rodgers Plumbing and Heating offers a variety of services to our customers.</p>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<img class="d-block w-100" src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="Second slide">
+			<div class="carousel-caption d-none d-md-block">
+				<h5>Utility Piping</h5>
+				<p>We offer installation services for gas piping, water piping, and sewer piping. We are licensed to work
+					on utility lines from the street to your home or business.</p>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<img class="d-block w-100" src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="Third slide">
+			<div class="carousel-caption d-none d-md-block">
+				<h5>Fixture Installation/Repair</h5>
+				<p>We install toilets, sinks, bathtubs, garbage disposals, radiant heating, swamp coolers... If you need
+					it installed and it requires gas or water we will probably have you covered!</p>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<img class="d-block w-100" src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="Fourth slide">
+			<div class="carousel-caption d-none d-md-block">
+				<h5>Large Construction</h5>
+				<p>We do ground up construction work from Santa Fe to Las Cruces. We have worked hard over tha last forty
+					years to become one of the most competitive plumbing subcontractors around. contact us if you need a
+					price from our estimators for your next construction project!</p>
+			</div>
+		</div>
+		<div class="carousel-item">
+			<img class="d-block w-100" src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="Fifth slide">
+			<div class="carousel-caption d-none d-md-block">
+				<h5>Service Work</h5>
+				<p>We also have commercial and residential repair and troubleshooting available! Call us for scheduling
+					and pricing information!</p>
+			</div>
+		</div>
+	</div>
+	<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+		<span class="carousel-control-next-icon" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
+	</a>
+</div>
 
 </body>
 </html>
