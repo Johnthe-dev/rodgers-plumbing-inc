02715d23f283fe66aa480f07eae1793bb8440cf0
John Johnson-Rodgers <john@johnthe.dev>
2019-11-14 21:46:45 -0700

    tweaked index for mobile, adding form and composer and updating gitignore

--- index.php
+++ index.php
@@ -4,20 +4,38 @@
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
-			integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-	<link rel="stylesheet" href="styles.css">
+			integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous"/>
 
+	<!-- LINK YOUR CSS FILES DOWN HERE -->
+<link rel="stylesheet" href="styles.css">
 	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-			  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+			  integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
 			  crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-			  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"
+			  integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
 			  crossorigin="anonymous"></script>
+
+	<!-- jQuery Form, Additional Methods, Validate -->
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+			  integrity="sha384-vk5WoKIaW/vJyUAd9n/wmopsmNhiy+L2Z+SBxGYnUkunIxVxAv/UtMOhba/xskxh"
+			  crossorigin="anonymous"></script>
+	<script type="text/javascript"
+			  src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+	<script type="text/javascript"
+			  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+	<script type="text/javascript"
+			  src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
+
+	<!-- Your JavaScript Form Validator -->
+	<script src="js/form-validate.js"></script>
+
+	<!-- Google reCAPTCHA -->
+	<script src='https://www.google.com/recaptcha/api.js'></script>
+	<!--title-->
 	<title>Rodgers Plumbing and Heating</title>
 </head>
 <body>
@@ -45,15 +63,16 @@
 </div>
 
 <!--todo make navbar sticky move to header?-->
-<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
-	<a class="navbar-brand" href="#top"><img src="http://rphi.biz/images/rphi001002.png"
-														  alt="logo for Rodgers Plumbing and Heating"></a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between sticky-top">
+	<a class="navbar-brand" href="#top">
+		<img src="http://rphi.biz/images/rphi001002.png" alt="logo for Rodgers Plumbing and Heating">
+	</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
 			  aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
 	<div class="collapse navbar-collapse" id="navbarNav">
-		<ul class="navbar-nav">
+		<ul class="navbar-nav bg-light">
 			<li class="nav-item active">
 				<a class="nav-link" href="#about">About Us <span class="sr-only">(current)</span></a>
 			</li>
@@ -67,9 +86,9 @@
 	</div>
 </nav>
 <div class="container" id="about">
-	<div class="row pt-5">
+	<div class="row align-items-center py-5">
 		<div class="col-md-6">
-			<img src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="Stand-In Picture">
+			<img src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="Stand-In Picture" class="img-responsive mw-100">
 		</div>
 		<div class="col-md-6">
 			<p>We are a locally owned and operated plumbing and heating company serving Albuquerque, Santa Fe and the rest
@@ -89,7 +108,7 @@
 				hope to continue to do so for many more.</p>
 		</div>
 		<div class="col-md-6">
-			<img src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="Stand-In Picture">
+			<img src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="Stand-In Picture" class="img-responsive mw-100">
 		</div>
 	</div>
 </div>
@@ -104,31 +123,51 @@
 	</ol>
 	<div class="carousel-inner" id="services">
 		<div class="carousel-item active">
-			<img class="d-block w-100" src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="First slide">
-			<div class="carousel-caption d-none d-md-block" id="caption">
+			<img class="d-block w-100" src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="First slide"  class="img-responsive">
+			<div class="carousel-caption d-none d-lg-block" id="caption">
+				<h5>Services We Provide</h5>
+				<p>Rodgers Plumbing and Heating offers a variety of services to our customers.</p>
+			</div>
+			<div class="d-lg-none" id="caption-md">
 				<h5>Services We Provide</h5>
 				<p>Rodgers Plumbing and Heating offers a variety of services to our customers.</p>
 			</div>
 		</div>
 		<div class="carousel-item">
-			<img class="d-block w-100" src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="Second slide">
-			<div class="carousel-caption d-none d-md-block" id="caption">
+			<img class="d-block w-100" src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="Second slide" class="img-responsive">
+			<div class="carousel-caption d-none d-lg-block" id="caption">
+				<h5>Utility Piping</h5>
+				<p>We offer installation services for gas piping, water piping, and sewer piping. We are licensed to work
+					on utility lines from the street to your home or business.</p>
+			</div>
+			<div class="d-lg-none" id="caption-md">
 				<h5>Utility Piping</h5>
 				<p>We offer installation services for gas piping, water piping, and sewer piping. We are licensed to work
 					on utility lines from the street to your home or business.</p>
 			</div>
 		</div>
 		<div class="carousel-item">
-			<img class="d-block w-100" src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="Third slide">
-			<div class="carousel-caption d-none d-md-block" id="caption">
+			<img class="d-block w-100" src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="Third slide" class="img-responsive">
+			<div class="carousel-caption d-none d-lg-block" id="caption">
+				<h5>Fixture Installation/Repair</h5>
+				<p>We install toilets, sinks, bathtubs, garbage disposals, radiant heating, swamp coolers... If you need
+					it installed and it requires gas or water we will probably have you covered!</p>
+			</div>
+			<div class="d-lg-none" id="caption-md">
 				<h5>Fixture Installation/Repair</h5>
 				<p>We install toilets, sinks, bathtubs, garbage disposals, radiant heating, swamp coolers... If you need
 					it installed and it requires gas or water we will probably have you covered!</p>
 			</div>
 		</div>
 		<div class="carousel-item">
-			<img class="d-block w-100" src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="Fourth slide">
-			<div class="carousel-caption d-none d-md-block" id="caption">
+			<img class="d-block w-100" src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="Fourth slide" class="img-responsive">
+			<div class="carousel-caption d-none d-lg-block" id="caption">
+				<h5>Large Construction</h5>
+				<p>We do ground up construction work from Santa Fe to Las Cruces. We have worked hard over tha last forty
+					years to become one of the most competitive plumbing subcontractors around. contact us if you need a
+					price from our estimators for your next construction project!</p>
+			</div>
+			<div class="d-lg-none" id="caption-md">
 				<h5>Large Construction</h5>
 				<p>We do ground up construction work from Santa Fe to Las Cruces. We have worked hard over tha last forty
 					years to become one of the most competitive plumbing subcontractors around. contact us if you need a
@@ -136,31 +175,95 @@
 			</div>
 		</div>
 		<div class="carousel-item">
-			<img class="d-block w-100" src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="Fifth slide">
-			<div class="carousel-caption d-none d-md-block" id="caption">
+			<img class="d-block w-100" src="sams-solutions-qsk_ifUucWE-unsplash.jpg" alt="Fifth slide" class="img-responsive">
+			<div class="carousel-caption d-none d-lg-block" id="caption">
+				<h5>Service Work</h5>
+				<p>We also have commercial and residential repair and troubleshooting available! Call us for scheduling
+					and pricing information!</p>
+			</div>
+			<div class="d-lg-none" id="caption-md">
 				<h5>Service Work</h5>
 				<p>We also have commercial and residential repair and troubleshooting available! Call us for scheduling
 					and pricing information!</p>
 			</div>
 		</div>
 	</div>
-	<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+	<a class="carousel-control-prev d-none d-md-block" href="#carouselExampleIndicators" role="button" data-slide="prev">
 		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 		<span class="sr-only">Previous</span>
 	</a>
-	<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+	<a class="carousel-control-next d-none d-md-block" href="#carouselExampleIndicators" role="button" data-slide="next">
 		<span class="carousel-control-next-icon" aria-hidden="true"></span>
 		<span class="sr-only">Next</span>
 	</a>
 </div>
 <!-- add in bootstrap form-->
-<footer class="container" id="contact">
-	<div class="row pt-5">
-		<div class="col-7">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3268.043400548775!2d-106.66014908466121!3d35.005618680357344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87220e998260bb93%3A0xa22064ae417f851f!2s5105%20William%20St%20SE%2C%20Albuquerque%2C%20NM%2087105!5e0!3m2!1sen!2sus!4v1573443477953!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+<footer class="container-fluid " id="contact">
+	<div class="row py-5 d-flex justify-content-around">
+
+
+		<div class="col-5-md">
+			<iframe
+				src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3268.043400548775!2d-106.66014908466121!3d35.005618680357344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87220e998260bb93%3A0xa22064ae417f851f!2s5105%20William%20St%20SE%2C%20Albuquerque%2C%20NM%2087105!5e0!3m2!1sen!2sus!4v1573443477953!5m2!1sen!2sus"
+				width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 		</div>
-		<div class="col-5">
-			<img src="https://miro.medium.com/max/721/1*gq9iyNw_4QVIlZkLDeWo0A.png" class="w-100" alt="filler image for form"/>
+		<div class="col-6-md">
+			<form id="johns-form" name="johns-form" action="../php/" method="post">
+				<div class="form-group">
+					<label for="name">Name <span class="text-danger">*</span></label>
+					<div class="input-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"></span>
+							<i class="fa fa-user" aria-hidden="true"></i>
+						</div>
+						<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="email">Email <span class="text-danger">*</span></label>
+					<div class="input-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"></span>
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</div>
+						<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="subject">Subject</label>
+					<div class="input-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"></span>
+							<i class="fa fa-pencil" aria-hidden="true"></i>
+						</div>
+						<input type="text" class="form-control" id="subject" name="subject"
+								 placeholder="Subject">
+					</div>
+				</div>
+				<div class="form-group">
+					<label for="message">Message <span class="text-danger">*</span></label>
+					<div class="input-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"></span>
+							<i class="fa fa-comment" aria-hidden="true"></i>
+						</div>
+						<textarea class="form-control" rows="5" id="message" name="message"
+									 placeholder="(2000 characters max)"></textarea>
+					</div>
+				</div>
+
+				<!-- reCAPTCHA -->
+				<div class="g-recaptcha" data-sitekey="6LewKsIUAAAAAEurxqpHO3Cyt0nDVEZXG3wwV4Sx"></div>
+
+				<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+				<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+			</form>
+		</div>
+		<!--empty area for form error/success output-->
+		<div class="row">
+			<div class="col-xs-12">
+				<div id="output-area"></div>
+			</div>
 		</div>
 	</div>
 </footer>
