df1248517ddf60df8ba3dae4eef7c3245f54bba1
John Johnson-Rodgers <john@johnthe.dev>
2019-11-17 10:32:06 -0700

    added boxshadow to navbar

--- public_html/index.php
+++ public_html/index.php
@@ -53,7 +53,7 @@
 
 
 <!--todo make navbar sticky move to header?-->
-<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between sticky-top">
+<nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between sticky-top" id="navBar">
 	<a class="navbar-brand" href="#top">
 		<img src="http://rphi.biz/images/rphi001002.png" alt="logo for Rodgers Plumbing and Heating">
 	</a>
@@ -210,7 +210,7 @@
 				src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3268.043400548775!2d-106.66014908466121!3d35.005618680357344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87220e998260bb93%3A0xa22064ae417f851f!2s5105%20William%20St%20SE%2C%20Albuquerque%2C%20NM%2087105!5e0!3m2!1sen!2sus!4v1573443477953!5m2!1sen!2sus"
 				width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 		</div>
-		<div class="col-md-6 py-4">
+		<div class="col-md-5 py-4">
 			<form id="johns-form" name="johns-form" action="../php/" method="post">
 				<div class="form-group">
 					<label for="name">Name <span class="text-danger">*</span></label>
