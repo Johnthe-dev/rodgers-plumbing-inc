8007a42a6c6baffe35e50b95afae4214dfce6784
John Johnson-Rodgers <john@johnthe.dev>
2019-11-18 11:27:01 -0700

    fixed carousel control styles

--- public_html/index.php
+++ public_html/index.php
@@ -193,12 +193,12 @@
 			</div>
 		</div>
 	</div>
-	<a class="carousel-control-prev d-none d-md-block" href="#carouselExampleIndicators" role="button" data-slide="prev">
-		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	<a class="carousel-control-prev d-none d-md-block" href="#carouselExampleIndicators" role="button" data-slide="prev" id="previous">
+		<span class="carousel-control-prev-icon rounded" aria-hidden="true"></span>
 		<span class="sr-only">Previous</span>
 	</a>
-	<a class="carousel-control-next d-none d-md-block" href="#carouselExampleIndicators" role="button" data-slide="next">
-		<span class="carousel-control-next-icon" aria-hidden="true"></span>
+	<a class="carousel-control-next d-none d-md-block" href="#carouselExampleIndicators" role="button" data-slide="next" id="next">
+		<span class="carousel-control-next-icon rounded" aria-hidden="true"></span>
 		<span class="sr-only">Next</span>
 	</a>
 </div>
